7d42aed95bbee8ec0da54841e0b53cb1624ed5ab
MaydaZH <flaquita2041@gmail.com>
2020-08-24 19:26:21 -0500

    version 1

---
+++ futboleros/app/src/main/java/com/mayda/aplicaciondiseno/ActAutenticacion.java
@@ -0,0 +1,14 @@
+package com.mayda.aplicaciondiseno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActAutenticacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_autenticacion);
+    }
+}
---
+++ futboleros/app/src/main/java/com/mayda/aplicaciondiseno/crearPartido.java
@@ -0,0 +1,14 @@
+package com.mayda.aplicaciondiseno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class crearPartido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_partido);
+    }
+}
